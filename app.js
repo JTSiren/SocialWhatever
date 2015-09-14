82d1145d93429f2ec10cd2d1a17aba4fece36815
Joe Fraley <jose.fraley@gmail.com>
2015-09-14 11:20:07 -0700

    initial skeleton

---
+++ app.js
@@ -0,0 +1,61 @@
+//=====================================
+//  Initialization
+//=====================================
+
+var express = require('express');
+var app = express();
+
+
+
+//=====================================
+//  Routes
+//=====================================
+
+app.get('/', function(req, res){
+  res.sendfile('public/index.html');
+});
+
+app.post('/login', function(req, res){
+  //
+})
+
+app.get('/register', function(req, res){
+
+})
+
+app.post('/register', function(req, res){
+  
+})
+
+app.get('/search', function(req, res){
+
+})
+
+app.post('/search', function(req, res){
+  
+})
+
+app.get('/profile', function(req, res){
+  //
+});
+
+app.get('/about', function(req, res){
+  
+});
+
+app.get('/favorites', function(req, res){
+
+})
+
+
+
+//=====================================
+//  Server / Host Instructions
+//=====================================
+
+var server = app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Our server is running at http://%s:%s', host, port);
+});
